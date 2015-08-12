058d654b6721bc5f6e77bbe0dd456b16b9f8c73d
444ten <444ten@Chas.local>
2015-08-12 09:02:34 +0300

    project -> initial commit

---
+++ TENCoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TENCoreData
+//
+//  Created by 444ten on 8/12/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TENCoreData/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TENCoreData
+//
+//  Created by 444ten on 8/12/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
